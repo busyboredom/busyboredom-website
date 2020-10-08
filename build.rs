660409073d3ca7030bde076c15c95b12b1f5c8a1
Charlie Wilkin <cwilkin@protonmail.com>
2020-10-07 21:18:36 -0400

    Update dependencies

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 use std::process::Command;
 
 fn main() {
-    Command::new("wasm-pack").args(&["build", "wasm/", "--release", "--target", "web"])
-        .status().unwrap();
+    Command::new("wasm-pack")
+        .args(&["build", "wasm/", "--release", "--target", "web"])
+        .status()
+        .unwrap();
     println!("cargo:rerun-if-changed=wasm/*");
 }
